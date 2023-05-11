9b01115aa86ebb49506106def3ad56890e80a5aa
Patrick Tumu <patricktumu@gmail.com>
2023-05-10 18:29:22 -0600

    added the ability for user to move snake using wasd keys also added the ability to output canned messages this was a good lesson in global scope and passing data through callbacks

--- input.js
+++ input.js
@@ -1,13 +1,10 @@
-const handleUserInput = function(key) {
-  // your code here
-  if (key === '\u0003') {
-    process.exit();
-  }
-};
+// Stores the active TCP connection object.
+let connection;
 
 // setup interface to handle user input from stdin
 
-const setupInput = function() {
+const setupInput = function(conn) {
+  connection = conn;
   const stdin = process.stdin;
   stdin.setRawMode(true);
   stdin.setEncoding("utf8");
@@ -18,5 +15,39 @@ const setupInput = function() {
 
   return stdin;
 };
+
+
+
+const handleUserInput = function(key) {
+  // your code here
+  switch (key) {
+  case '\u0003':
+    process.exit();
+    break;
+  case "w":
+    connection.write("Move: up");
+    break;
+  case "a":
+    connection.write("Move: left");
+    break;
+  case "d":
+    connection.write("Move: right");
+    break;
+  case "s":
+    connection.write("Move: down");
+    break;
+  case "b":
+    connection.write("Say: BUMBLEBEETUNA!");
+    break;
+  case "q":
+    process.exit();
+    break;
+  default:
+    break;
+  }
+
+};
+
+
 
 module.exports = { setupInput };
--- play.js
+++ play.js
@@ -2,7 +2,6 @@ const { setupInput } = require('./input.js');
 const { connect } = require("./client");
 
 console.log("Connecting ...");
-connect();
 
-setupInput();
+setupInput(connect());
 
